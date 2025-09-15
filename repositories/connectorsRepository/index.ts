b0026f143f87848e8d68aa567f2ac869cff82b3e
alexpavlowv <riashenka@gmail.com>
2025-09-15 23:31:54 +0700

    fix

--- repositories/connectorsRepository/index.ts
+++ repositories/connectorsRepository/index.ts
@@ -1,14 +1,67 @@
+import { Status } from "../../types/status";
 import { IVisit } from "../../types/visit";
 import { IVisitDialogMessage } from "../../types/visitDialogMessage";
-import { IVisitFeedback } from "../../types/visitFeedback";
+import { IVisitFeedback, VisitFeedbackType } from "../../types/visitFeedback";
 import { IVisitRate } from "../../types/visitRate";
 
 class ConnectorsRepository {
   constructor() {}
 
-  async saveFeedback(visit: IVisitFeedback, visitDialogMessages: IVisitDialogMessage[]): Promise<boolean> {
-    return true;
+  async saveFeedback(
+    visit: IVisit,
+    visitFeedback: IVisitFeedback,
+    visitDialogMessages: IVisitDialogMessage[]
+  ): Promise<boolean> {
+    try {
+      switch (visitFeedback.type) {
+        case VisitFeedbackType.Positive: {
+          const res = await fetch(process.env.GOOGLE_DOCS_CONNECTOR_URL + "/api/feedback", {
+            method: "POST",
+            body: JSON.stringify({
+              type: visitFeedback.type,
+              summary: visitFeedback.summary,
+              dialog: visitDialogMessages,
+              patient: visit.child || visit.parent,
+              phone: visit.phone,
+              date: visit.date,
+              processedAt: visit.processedAt,
+              doctor: visit.doctor,
+              address: visit.address,
+            }),
+          });
+
+          const json = await res.json();
+
+          return json.status === Status.Success;
+        }
+
+        case VisitFeedbackType.Negative: {
+          const res = await fetch(process.env.ONE_FORMA_CONNECTOR_URL + "/api/feedback", {
+            method: "POST",
+            body: JSON.stringify({
+              type: visitFeedback.type,
+              summary: visitFeedback.summary,
+              dialog: visitDialogMessages,
+              fullname: visit.child || visit.parent,
+              phone: visit.phone,
+              date: visit.date,
+            }),
+          });
+
+          const json = await res.json();
+
+          return json.status === Status.Success;
+        }
+      }
+
+      return false;
+    } catch (e) {
+      console.log(e);
+      return false;
+    }
   }
+
+  //     address: Joi.string().min(1).required(),
 
   async saveProtocol(visit: IVisit): Promise<boolean> {
     return true;
